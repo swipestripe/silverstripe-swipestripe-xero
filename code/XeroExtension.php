1758653b638bdabffeeec577ef6d432164c8aea4
Frank Mullenger <frank@silverstripe.com>
2014-02-04 17:03:01 +1300

    NEW: Adding payments for invoices.

---
+++ code/XeroExtension.php
@@ -0,0 +1,22 @@
+<?php
+
+class XeroExtension_Customer extends DataExtension {
+
+	private static $db = array(
+		'XeroContactID' => 'Varchar(255)'
+	);
+}
+
+class XeroExtension_Order extends DataExtension {
+
+	private static $db = array(
+		'XeroInvoiceID' => 'Varchar(255)'
+	);
+}
+
+class XeroExtension_Payment extends DataExtension {
+
+	private static $db = array(
+		'XeroPaymentID' => 'Varchar(255)'
+	);
+}
--- tasks/CreateInvoicesTask.php
+++ tasks/CreateInvoicesTask.php
@@ -8,9 +8,9 @@ class RemoveDevOrdersTask extends BuildTask {
 	
 	protected $description = 'Create invoices in Xero for SwipeStripe orders.';
 
-	function run($request) {
+	public function run($request) {
 
-
+		// TODO: Configuration values
 		define ( "XRO_APP_TYPE", "Private" );
 		define ( "OAUTH_CALLBACK", "oob" );
 		$useragent = "XeroOAuth-PHP Private App Test";
@@ -58,48 +58,232 @@ class RemoveDevOrdersTask extends BuildTask {
 				$XeroOAuth->config ['access_token'] = $oauthSession ['oauth_token'];
 				$XeroOAuth->config ['access_token_secret'] = $oauthSession ['oauth_token_secret'];
 				
-				$this->createInvoice($XeroOAuth);
-				// include 'tests/tests.php';
+				$this->createInvoices($XeroOAuth);
+				$this->createPayments($XeroOAuth);
 			}
-			
-			// testLinks ();
 		}
 	}
 
-	private function createInvoice($XeroOAuth) {
-		$xml = "<Invoices>
-							<Invoice>
-								<Type>ACCREC</Type>
-								<Contact>
-									<Name>Martin Hudson</Name>
-								</Contact>
-								<Date>2013-05-13T00:00:00</Date>
-								<DueDate>2013-05-20T00:00:00</DueDate>
-								<LineAmountTypes>Exclusive</LineAmountTypes>
-								<LineItems>
-									<LineItem>
-										<Description>Monthly rental for property at 56a Wilkins Avenue</Description>
-										<Quantity>4.3400</Quantity>
-										<UnitAmount>395.00</UnitAmount>
-										<AccountCode>200</AccountCode>
-									</LineItem>
-							 </LineItems>
-						 </Invoice>
-					 </Invoices>";
-		$response = $XeroOAuth->request('POST', $XeroOAuth->url('Invoices', 'core'), array(), $xml);
-		if ($XeroOAuth->response['code'] == 200) {
-				$invoice = $XeroOAuth->parseResponse($XeroOAuth->response['response'], $XeroOAuth->response['format']);
-				echo "" . count($invoice->Invoices[0]). " invoice created in this Xero organisation.";
-				if (count($invoice->Invoices[0])>0) {
+	private function createInvoices($XeroOAuth) {
 
-					SS_Log::log(new Exception(print_r($invoice->Invoices[0]->Invoice, true)), SS_Log::NOTICE);
-					// echo "The first one is: </br>";
-					// pr($invoice->Invoices[0]->Invoice);
-					// outputError($XeroOAuth);
+		$xeroConnection = clone $XeroOAuth;
+
+		// TODO: Configuration values
+		$defaultAccountCode = 200;
+		$defaultTaxType = 'OUTPUT2';
+		$zeroTaxTaype = 'NONE';
+		$taxClasses = array(
+			'FlatFeeTaxModification'
+		);
+		$baseCurrency = 'NZD';
+
+
+		$invoices = array();
+		$orders = Order::get()->where(" \"XeroInvoiceID\" IS NULL");
+		$i = 0;
+
+		if ($orders && $orders->exists()) foreach ($orders as $order) {
+
+			// TODO: Configuration values
+			$invoicePrefix = 'WEB-';
+
+			$invoices[$i]['Invoice'] = array(
+				'Type' => 'ACCREC',
+				'InvoiceNumber' => $invoicePrefix . $order->ID,
+				'Contact' => array(
+					'Name' => $order->Member()->getName()
+				),
+				'Date' => $order->OrderedOn,
+				'DueDate' => $order->OrderedOn,
+				'Status' => 'AUTHORISED',
+				'LineAmountTypes' => 'Exclusive',
+				'CurrencyCode' => $baseCurrency
+			);
+
+			// Line items for each item in the order
+			$items = $order->Items();
+			if ($items && $items->exists()) foreach ($items as $item) {
+
+				$object = ($item->Variation()) ? $item->Variation() : $item->Product();
+
+				$description = $object->Title;
+				if ($object instanceof Variation) {
+					$description = strip_tags($object->Product()->Title . ' ' . $object->SummaryOfOptions());
 				}
-		} else {
-			SS_Log::log(new Exception(print_r($XeroOAuth, true)), SS_Log::NOTICE);
-			// outputError($XeroOAuth);
+
+				$invoices[$i]['Invoice']['LineItems'][]['LineItem'] = array(
+					'Description' => $description,
+					'Quantity' => $item->Quantity,
+					'UnitAmount' => $item->Total()->getAmount(),
+					'AccountCode' => $defaultAccountCode,
+					'TaxType' => $defaultTaxType
+				);
+			}
+
+			// Line items for each order modifier
+			$modifications = $order->Modifications();
+			if ($modifications && $modifications->exists()) foreach ($modifications as $modification) {
+
+				if ($modification->SubTotalModifier) {
+					$invoices[$i]['Invoice']['LineItems'][]['LineItem'] = array(
+						"Description" => $modification->Description,
+						"Quantity" => 1,
+						"UnitAmount" => $modification->Amount()->getAmount(),
+						"AccountCode" => $defaultAccountCode,
+						'TaxType' => $defaultTaxType
+					);
+				}
+				else if (!in_array(get_class($modification), $taxClasses)) {
+					$invoices[$i]['Invoice']['LineItems'][]['LineItem'] = array(
+						"Description" => $modification->Description,
+						"Quantity" => 1,
+						"UnitAmount" => $modification->Amount()->getAmount(),
+						"AccountCode" => $defaultAccountCode,
+						'TaxType' => $zeroTaxTaype
+					);
+				}
+			}
+
+			$i++;
 		}
+
+		if (empty($invoices)) {
+			return;
+		}
+
+		// SS_Log::log(new Exception(print_r($invoices, true)), SS_Log::NOTICE);
+
+		$invoicesXML = new SimpleXMLElement("<Invoices></Invoices>");
+		$this->arrayToXML($invoices, $invoicesXML);
+		$xml = $invoicesXML->asXML();
+
+		// SS_Log::log(new Exception(print_r($this->prettyPrintXML($xml), true)), SS_Log::NOTICE);
+
+		$response = $xeroConnection->request('POST', $xeroConnection->url('Invoices', 'core'), array(), $xml);
+		if ($xeroConnection->response['code'] == 200) {
+
+			$invoices = $xeroConnection->parseResponse($xeroConnection->response['response'], $xeroConnection->response['format']);
+
+			echo count($invoices->Invoices[0]). " invoice(s) created in this Xero organisation.";
+
+			// SS_Log::log(new Exception(print_r($invoices, true)), SS_Log::NOTICE);
+
+			foreach ($invoices->Invoices->Invoice as $invoice) {
+
+				// Match the invoice number and update XeroInvoiceID so that it is not sent to Xero again
+				$order = Order::get()
+					->filter('ID', str_replace($invoicePrefix, '', $invoice->InvoiceNumber->__toString()))
+					->first();
+
+				if ($order && $order->exists()) {
+					$order->XeroInvoiceID = $invoice->InvoiceID->__toString();
+					$order->write();
+				}
+			}
+		}
+		else {
+			echo 'Error: ' . $xeroConnection->response['response'] . PHP_EOL;
+			SS_Log::log(new Exception(print_r($xeroConnection, true)), SS_Log::NOTICE);
+		}
+	}
+
+	private function createPayments($XeroOAuth) {
+
+		$xeroConnection = clone $XeroOAuth;
+
+		// TODO: Configuration values
+		$defaultAccountPurchasesCode = '090';
+
+		$data = array();
+		$orders = Order::get()->where(" \"XeroInvoiceID\" IS NOT NULL");
+		$i = 0;
+
+		if ($orders && $orders->exists()) foreach ($orders as $order) {
+
+			$payments = $order->Payments();
+			if ($payments && $payments->exists()) foreach ($payments as $payment) {
+
+				if ($payment->XeroPaymentID) {
+					continue;
+				}
+
+				$data[$i]['Payment'] = array(
+					'Invoice' => array(
+						'InvoiceID' => $order->XeroInvoiceID
+					),
+					'Account' => array(
+						'Code' => $defaultAccountPurchasesCode
+					),
+					'Date' => date('Y-m-d', strtotime($payment->Created)),
+					'Amount' => $payment->Amount->getAmount()
+				);
+
+				$i++;
+			}
+		}
+
+		$paymentsXML = new SimpleXMLElement("<Payments></Payments>");
+		$this->arrayToXML($data, $paymentsXML);
+		$xml = $paymentsXML->asXML();
+
+// 		$xml = "
+// <Payments>
+//   <Payment>
+//     <Invoice>
+//       <InvoiceID>5a88b23d-ea81-4589-a4e1-3be92f3f195e</InvoiceID>
+//     </Invoice>
+//     <Account>
+//       <Code>090</Code>
+//     </Account>
+//     <Date>2014-02-04</Date>
+//     <Amount>72.67</Amount>
+//   </Payment>
+// </Payments>
+// 		";
+
+		SS_Log::log(new Exception(print_r($this->prettyPrintXML($xml), true)), SS_Log::NOTICE);
+
+		SS_Log::log(new Exception(print_r($xeroConnection->url('Payments', 'core'), true)), SS_Log::NOTICE);
+
+		$response = $xeroConnection->request('POST', $xeroConnection->url('Payments', 'core'), array(), $xml);
+
+		SS_Log::log(new Exception(print_r($response, true)), SS_Log::NOTICE);
+
+		if ($xeroConnection->response['code'] == 200) {
+
+			$payments = $xeroConnection->parseResponse($xeroConnection->response['response'], $xeroConnection->response['format']);
+		}
+		else {
+			echo 'Error: ' . $xeroConnection->response['response'] . PHP_EOL;
+			SS_Log::log(new Exception(print_r($xeroConnection, true)), SS_Log::NOTICE);
+		}
+	}
+
+	private function arrayToXML($data, &$xml) {
+
+		foreach($data as $key => $value) {
+
+			if(is_array($value)) {
+				if(!is_numeric($key)){
+					$subnode = $xml->addChild("$key");
+					$this->arrayToXML($value, $subnode);
+				}
+				else{
+					$this->arrayToXML($value, $xml);
+				}
+			}
+			else {
+				$xml->addChild("$key", "$value");
+			}
+		}
+	}
+
+	private function prettyPrintXML($xml) {
+
+		$domxml = new DOMDocument('1.0');
+		$domxml->preserveWhiteSpace = false;
+		$domxml->formatOutput = true;
+		$domxml->loadXML($xml);
+		return $domxml->saveXML();
 	}
 }
